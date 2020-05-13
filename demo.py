acbffecc50be1c8ce8fda95527d7b4fa0085c60a
yang123qwe <31727996+yang123qwe@users.noreply.github.com>
2020-05-13 14:14:24 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+import numpy as np
+aishell_1_sou = np.load('data/aishell_1_sou.npy').item()
+aishell_1_yin_s = np.load('data/aishell_1_yin_s.npy').item()
+print (aishell_1_sou)
+print (aishell_1_yin_s)
